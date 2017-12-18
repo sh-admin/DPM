0c624d60a72a505e15044bf7f3905280a984fd42
Chintha Hansani <mchansani@virtusa.com>
2017-12-18 16:15:44 +0530

    v1 with doctor and user model classes

---
+++ dpm-master/src/main/java/com/virtusa/dpm_master/model/Doctor.java
@@ -0,0 +1,51 @@
+package com.virtusa.dpm_master.model;
+
+public class Doctor {
+
+	private int docId;
+	private String docName;
+	private String docSpecArea;
+	private String mobileNumber;
+	private String address;
+
+	public int getDocId() {
+		return docId;
+	}
+
+	public void setDocId(int docId) {
+		this.docId = docId;
+	}
+
+	public String getDocName() {
+		return docName;
+	}
+
+	public void setDocName(String docName) {
+		this.docName = docName;
+	}
+
+	public String getDocSpecArea() {
+		return docSpecArea;
+	}
+
+	public void setDocSpecArea(String docSpecArea) {
+		this.docSpecArea = docSpecArea;
+	}
+
+	public String getMobileNumber() {
+		return mobileNumber;
+	}
+
+	public void setMobileNumber(String mobileNumber) {
+		this.mobileNumber = mobileNumber;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+}
