cee701dba9ac454aabdf22c727efd63ffa77e7fd
Chintha Hansani <mchansani@virtusa.com>
2017-12-21 14:55:32 +0530

    commit the service package

---
+++ dpm-master/src/main/java/com/virtusa/dpm_master/service/DoctorService.java
@@ -0,0 +1,7 @@
+package com.virtusa.dpm_master.service;
+
+import com.virtusa.dpm_master.model.Doctor;
+
+public interface DoctorService {
+	public void addDoctor(Doctor doctor);
+}
---
+++ dpm-master/src/main/java/com/virtusa/dpm_master/service/DoctorServiceImpl.java
@@ -0,0 +1,18 @@
+package com.virtusa.dpm_master.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.virtusa.dpm_master.model.Doctor;
+import com.virtusa.dpm_master.repository.DoctorRepository;
+@Service
+public class DoctorServiceImpl implements DoctorService {
+	
+	@Autowired
+	DoctorRepository doctorRepository;
+	
+	public void addDoctor(Doctor doctor) {
+		doctorRepository.save(doctor);
+	}
+
+}
